709b7f6f8c7f19eb5a801792da79dba1b2b529e5
miradii <mrtuz@protonmail.com>
2020-12-01 21:32:38 +0330

    completed UI functionality

--- src/rps.js
+++ src/rps.js
@@ -1,70 +1,130 @@
+//@format
 const weapon = {
-    rock: {
-        name: 'rock',
-        vs(otherWeapon) {
-            return otherWeapon === this.paper
-                ? [false, 'Paper covers rock']
-                : [true, 'Rock curshes scissors']
-        },
-    },
-    paper: {
-        name: 'paper',
-        vs(otherWeapon) {
-            return otherWeapon === this.rock
-                ? [true, 'Paper covers rock']
-                : [false, 'Scissors cut paper']
-        },
-    },
-    scissors: {
-        name: 'scissors',
-        vs(otherWeapon) {
-            return otherWeapon === this.paper
-                ? [true, 'Scissors cut paper']
-                : [false, 'Scissors cut paper']
-        },
-    },
+	rock: {
+		name: 'rock',
+		vs(otherWeapon) {
+			return otherWeapon.name === 'scissors'
+				? [true, 'Rock curshes scissors']
+				: [false, 'Paper covers rock']
+		},
+	},
+	paper: {
+		name: 'paper',
+		vs(otherWeapon) {
+			return otherWeapon.name === 'rock'
+				? [true, 'Paper covers rock']
+				: [false, 'Scissors cuts paper']
+		},
+	},
+	scissors: {
+		name: 'scissors',
+		vs(otherWeapon) {
+			return otherWeapon.name === 'paper'
+				? [true, 'Scissors cuts paper']
+				: [false, 'Rock curshes scissors']
+		},
+	},
 }
 
 /* generates random integer up to max */
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max))
+	return Math.floor(Math.random() * Math.floor(max))
 }
 /* a function that takes no input and returns random weapon string */
 function computerPlay() {
-    const weapons = ['rock', 'paper', 'scissors']
-    return weapon[weapons[getRandomInt(3)]]
+	const weapons = ['rock', 'paper', 'scissors']
+	return weapon[weapons[getRandomInt(3)]]
 }
-const computerSelection = computerPlay()
-/* a function that takes two weapon strings and determines which one is the winner*/
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) return [undefined, 'It,s a draw']
-    const [playerwins, message] = playerSelection.vs(computerSelection)
-    let whoWon = ''
-    if (playerwins) whoWon = `You Won, ${message}`
-    else whoWon = `You Lost, ${message}`
-    return [playerwins, whoWon]
+/* this returns a palyable function and keeps score in closure*/
+function newGame() {
+	let playerScore = 0
+	let comScore = 0
+	let gameOver = false
+	return function playRound(playerSelection, computerSelection) {
+		if (gameOver)
+			return [playerwins, whoWon, playerScore, comScore, true, false]
+		if (playerSelection == computerSelection)
+			return [false, "It's a draw", playerScore, comScore, gameOver, true]
+		const [playerwins, message] = playerSelection.vs(computerSelection)
+		let whoWon = ''
+		if (playerwins) {
+			whoWon = `You Won, ${message}`
+			playerScore++
+		} else {
+			whoWon = `You Lost, ${message}`
+			comScore++
+		}
+		console.log(gameOver, playerScore, comScore)
+		if (playerScore === 5 || comScore === 5) gameOver = true
+		return [playerwins, whoWon, playerScore, comScore, gameOver, false]
+	}
 }
-function game() {
-    let playerScore = 0
-    let comScore = 0
 
-    while (comScore != 5 || playerScore != 5) {
-        let playerSelection = prompt('what are you?')
-        let computerSelection = computerPlay()
-        let [playerWins, message] = playRound(
-            weapon[playerSelection.toLowerCase()],
-            computerSelection
-        )
-
-        if (playerWins == undefined)
-            console.log(message + ' ' + 'p:' + playerScore + ' c:' + comScore)
-        else if (playerWins) {
-            playerScore++
-            console.log(message + ' ' + 'p:' + playerScore + ' c:' + comScore)
-        } else {
-            comScore++
-            console.log(`${message} p:${playerScore} c:${comScore}`)
-        }
-    }
+/* ========================================================================================
+ * dealing with the DOM here, need to start a game, play a round with the player selection and the generated
+ * computer selection, update the computer selection photo and results and report the winner when the game is over
+ * ========================================================================================
+ * */
+let currentRound
+const startButton = document.getElementById('start')
+function updateScore(com, user) {
+	document.getElementById('comScore-text').innerText = com
+	document.getElementById('userScore-text').innerText = user
 }
-game()
+function updateResult(message) {
+	document.getElementById('result-message').innerText = message
+}
+
+function startNewGame() {
+	hideComputerSelection()
+	currentRound = newGame()
+	updateScore(0, 0)
+	updateResult('New game started, choose your weapon')
+	addEvents()
+}
+
+function playCurrentRound(playerSelection) {
+	hideComputerSelection()
+	const computerSelection = computerPlay()
+	showComputerSelection(computerSelection)
+	const [playerWin, message, playerScore, comScore, gameOver] = currentRound(
+		playerSelection,
+		computerSelection
+	)
+	updateScore(comScore, playerScore)
+	if (gameOver) {
+		playerWin
+			? updateResult('Congrats! You Win.')
+			: updateResult('Game Over! You Lost.')
+		removeEvents()
+	} else {
+		updateResult(message)
+	}
+}
+startButton.addEventListener('click', startNewGame)
+
+function playEvent(event) {
+	playCurrentRound(weapon[event.target.id])
+}
+function addEvents() {
+	document.getElementById('scissors').addEventListener('click', playEvent)
+	document.getElementById('paper').addEventListener('click', playEvent)
+	document.getElementById('rock').addEventListener('click', playEvent)
+}
+function removeEvents() {
+	document.getElementById('rock').removeEventListener('click', playEvent)
+	document.getElementById('scissors').removeEventListener('click', playEvent)
+	document.getElementById('paper').removeEventListener('click', playEvent)
+}
+
+function showComputerSelection(selection) {
+	console.log('fake-' + selection.name)
+	const selected = document.getElementById('fake-' + selection.name)
+	console.log(selected)
+	selected.classList.remove('hidden')
+}
+function hideComputerSelection() {
+	const selected = document.querySelectorAll('.fake-btn')
+	console.log(selected)
+	selected.forEach((btn) => btn.classList.add('hidden'))
+}
